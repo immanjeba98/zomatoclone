b8b1d04a280c4ffbecb36982efa2a41020135e69
immanjeba98 <104260740+immanjeba98@users.noreply.github.com>
2022-12-07 17:07:30 +0530

    zomato clone
    zomato clone using react native

---
+++ sanity.js
@@ -0,0 +1,16 @@
+import  SanityClient  from "@sanity/client";
+import ImageUrlBuilder  from "@sanity/image-url";
+
+
+export const  client = SanityClient({
+    projectId:"tijpedt0",
+    dataset:"production",
+    apiVersion:"2022-03-10",
+    useCdn: true,
+})
+
+const builder = ImageUrlBuilder(client)
+
+export const urlFor = (source) => builder.image(source);
+
+export default client;
